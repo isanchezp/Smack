8a1788e4ec7d21654485f40de2cc652d36ea2834
isanchezp <ivansanchez.uam@gmail.com>
2021-05-13 15:22:53 -0500

    login activity design

---
+++ app/src/main/java/com/ivan/smack/LoginActivity.kt
@@ -0,0 +1,17 @@
+package com.ivan.smack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    fun onLoginClicked(view: View) {}
+
+    fun onSignupClicked(view: View) {}
+}
--- app/src/main/java/com/ivan/smack/MainActivity.kt
+++ app/src/main/java/com/ivan/smack/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.ivan.smack
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
     }
+
+    fun onLoginClicked(view: View) {
+        startActivity(Intent(this, LoginActivity::class.java))
+    }
+
+    fun onAddChannelClicked(view: View) {}
+
+    fun onSendClicked(view: View) {}
 }
