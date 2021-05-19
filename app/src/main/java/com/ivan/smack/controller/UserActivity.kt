3f02dd9337cf5c665e63ce7a2df1e2e047f9024c
isanchezp <ivansanchez.uam@gmail.com>
2021-05-19 12:59:19 -0500

    request error handling

--- app/src/main/java/com/ivan/smack/controller/UserActivity.kt
+++ app/src/main/java/com/ivan/smack/controller/UserActivity.kt
@@ -1,11 +1,15 @@
 package com.ivan.smack.controller
 
+import android.content.Intent
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.ivan.smack.R
 import com.ivan.smack.services.AuthService
+import com.ivan.smack.utils.BROADCAST_USER_DATA_CHANGE
 import kotlinx.android.synthetic.main.activity_login.*
 import kotlinx.android.synthetic.main.activity_user.*
 import java.util.*
@@ -18,6 +22,7 @@ class UserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
+        pb_loader.visibility = View.INVISIBLE
     }
 
     fun onAvatarClicked(view: View) {
@@ -41,7 +46,7 @@ class UserActivity : AppCompatActivity() {
         val g = random.nextInt(255)
         val b = random.nextInt(255)
 
-        iv_avatar.setBackgroundColor(Color.rgb(r,g,b))
+        iv_avatar.setBackgroundColor(Color.rgb(r, g, b))
 
         val savedR = r.toDouble() / 255
         val savedG = g.toDouble() / 255
@@ -51,21 +56,56 @@ class UserActivity : AppCompatActivity() {
     }
 
     fun onCreateUserClicked(view: View) {
+        enableSpinner(true)
+
         val userName = et_user_name.text.toString()
         val email = et_email.text.toString()
         val pws = et_key.text.toString()
-        AuthService.registerUser(this, email, pws){ registerSuccess ->
-            if (registerSuccess) {
-                AuthService.login(this, email, pws){ loginSuccess ->
-                    if (loginSuccess) {
-                        AuthService.createUser(this, userName, email, userAvatar, avatarColor){ createSuccess ->
-                            if (createSuccess) {
-                                finish()
+
+        if (userName.isNotEmpty() && email.isNotEmpty() && pws.isNotEmpty()) {
+            AuthService.registerUser(this, email, pws) { registerSuccess ->
+                if (registerSuccess) {
+                    AuthService.login(this, email, pws) { loginSuccess ->
+                        if (loginSuccess) {
+                            AuthService.createUser(
+                                this,
+                                userName,
+                                email,
+                                userAvatar,
+                                avatarColor
+                            ) { createSuccess ->
+                                if (createSuccess) {
+                                    val userDataChange = Intent(BROADCAST_USER_DATA_CHANGE)
+                                    LocalBroadcastManager.getInstance(this).sendBroadcast(userDataChange)
+                                    enableSpinner(false)
+                                    finish()
+                                } else {
+                                    errorToast()
+                                }
                             }
+                        } else {
+                            errorToast()
                         }
                     }
+                } else {
+                    errorToast()
                 }
             }
+        } else {
+            Toast.makeText(this, "Fit all the fields", Toast.LENGTH_LONG).show()
+            enableSpinner(false)
         }
+    }
+
+    fun enableSpinner(enable: Boolean) {
+        pb_loader.visibility = if (enable) View.VISIBLE else View.INVISIBLE
+        btn_create_user.isEnabled = !enable
+        iv_avatar.isEnabled = !enable
+        btn_color.isEnabled = !enable
+    }
+
+    fun errorToast() {
+        Toast.makeText(this, "Something went wrong, please try again", Toast.LENGTH_LONG).show()
+        enableSpinner(false)
     }
 }
